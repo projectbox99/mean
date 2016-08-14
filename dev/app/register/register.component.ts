2b3670b8a22faa26d3b8923a3cb18c3b2b5df44b
anapac <anapac@gmx.us>
2016-08-14 21:28:15 +0300

    registration form alpha

--- dev/app/register/register.component.ts
+++ dev/app/register/register.component.ts
@@ -1,16 +1,26 @@
 import { Component } from "@angular/core";
-import { NgForm } from "@angular/common"
+import { NgForm } from "@angular/common";
 import { Register, RegisterService } from "./register.service";
 
 @Component ({
     selector: "view",
-    templateUrl: "./register.component.html"
+    templateUrl: "./register.component.html",
+    styles: ['.ng-valid[required] { border-left: 5px solid #42A948; } .ng-invalid { border-left: 5px solid #a94442; }']
 })
 
-export class RegisterComponent { 
-    private submitted: boolean = false;
-    
+export class RegisterComponent {
+    constructor() {
+        this.model = new Register("", "", "", "", "", "", "", "", "", "", null);
+        this.active = true;
+    }
+
+    model: Register;
+    submitted: boolean = false;
+    active = true;
+
     onSubmit() {
         this.submitted = true;
     }
+
+    get diagnostic() { return JSON.stringify(this.model); }
 }
--- dev/app/register/register.service.ts
+++ dev/app/register/register.service.ts
@@ -15,21 +15,20 @@ const userUrl: string =
 
 export class Register {
     constructor(
-        public username: string = "",
-        public password: string = "",
-        public namesFirst: string = "",
-        public namesLast: string = "",
-        public email: string = "",
-        public phone1: string = "",
-        public phone2: string = "",
-        public skypeId: string = "",
-        public photo: string = "",
-        public role: string = "",
-        public dateCreated: Date = new Date) { }
-}
+        public username: string,
+        public password: string,
+        public namesFirst: string,
+        public namesLast: string,
+        public email: string,
+        public phone1: string,
+        public phone2: string,
+        public skypeId: string,
+        public photo: string,
+        public role: string,
+        public dateCreated: Date) { }
+}    // class Register
 
-@Injectable ()
-export class RegisterService{
+@Injectable()
+export class RegisterService {
     constructor (private http: Http) { }
-    
-}
+}    // class RegisterService
