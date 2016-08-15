25a4f595a4a624029cc18677142589d1af2a8132
nikolay-is <niki@vip.bg>
2016-08-15 18:56:41 +0300

    login form removed

--- dev/app/app.routes.ts
+++ dev/app/app.routes.ts
@@ -5,7 +5,6 @@ import { Routes, RouterModule } from "@angular/router";
 import { HomeComponent } from "./home/home.component";
 import { AboutComponent } from "./about/about.component";
 import { RegisterComponent } from "./register/register.component";
-import { LoginComponent } from "./login/login.component";
 import { PageNotFoundComponent } from "./404.component";
 
 const routes: Routes = [
