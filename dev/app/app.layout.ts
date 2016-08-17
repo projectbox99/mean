2d4b6b97f7682f2093e764e45b57861863657197
anapac <anapac@gmx.us>
2016-08-17 17:56:01 +0300

    login

--- dev/app/app.layout.ts
+++ dev/app/app.layout.ts
@@ -3,11 +3,21 @@
 import { Component } from "@angular/core";
 import { Router } from "@angular/router";
 
+import { User, UserService } from "./register/register.service";
+
 @Component ({
     selector: "mean2-app",
-    templateUrl: "./app.layout.html"
+    templateUrl: "./app.layout.html",
+    styleUrls: [ "./register/register.component.css" ],
+    providers: [ UserService ]
 })
-
 export class AppLayout {
-    constructor(private router: Router) { }
-}
+    constructor(private router: Router, private service: UserService) { }
+
+    private user: User = null;
+    private role: string = null;
+
+    public get isLoggedIn(): boolean {
+    	return this.user ? true : false;
+    }
+}	// class AppLayout
