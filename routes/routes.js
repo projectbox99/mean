46564133a5abf2aea3fabb680c5fd16f76d20e9e
anapac <anapac@gmx.us>
2016-08-21 19:11:12 +0300

    heroku

--- routes/routes.js
+++ routes/routes.js
@@ -7,7 +7,8 @@ var router = express.Router();
 
 /* GET/ALL home page. */
 router.all('/', function(req, res, next) {
-  res.sendFile(path.resolve('./public/index.html'));
+  console.log(`Serving index.html from: ${path.resolve(__dirname, '../public/index.html')}`);
+  res.sendFile(path.resolve(__dirname, '../public/index.html'));
 });
 
 module.exports = router;
