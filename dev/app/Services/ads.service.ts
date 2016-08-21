733d2784c39696560dc05b271250dde822224f9e
anapac <anapac@gmx.us>
2016-08-21 18:09:17 +0300

    heroku favicon

--- dev/app/Services/ads.service.ts
+++ dev/app/Services/ads.service.ts
@@ -3,7 +3,6 @@
 import { Injectable } from "@angular/core";
 import { Http, Headers, Response } from "@angular/http";
 
-import "rxjs/Rx";
 import { Observable } from "rxjs/Observable";
 
 
--- dev/app/Services/users.service.ts
+++ dev/app/Services/users.service.ts
@@ -6,7 +6,6 @@ import { Http, Headers, Response, RequestOptions } from "@angular/http";
 import { Observable } from "rxjs/Observable";
 import "rxjs/add/operator/map";
 import "rxjs/add/operator/catch";
-import "Rx";
 
 // import { AuthService } from "./authentication.service";
 
